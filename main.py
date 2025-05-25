39699c7e914d84bbc616fcdddea7b6b69ed82c2e
bambus <132773043+bambus80@users.noreply.github.com>
2025-05-25 16:21:28 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,120 @@
+#===========================================================================
+
+# Zadanie 1.
+# Dana jest lista liczb całkowitych dodatnich.
+# Znajdź najdłuższy podciąg rosnący (czyli taką sekwencję liczb,
+# w której każda kolejna liczba jest większa od poprzedniej)
+# w tej liście.
+#
+# Na przykład dla listy [10, 22, 9, 33, 21, 50, 41, 60, 80],
+# najdłuższy podciąg rosnący to 10, 22, 33, 50, 60, 80.
+
+print("Zadanie 1")
+
+input_arr = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+
+def zad1(arr: list[int]) -> list[int]:
+    i = 0
+    while i in range(0, len(arr) - 1):
+        if i > 0 and not arr[i] > arr[i - 1]:
+            arr.pop(i)
+        else:
+            i += 1
+    return arr
+
+print(zad1(input_arr))
+
+#===========================================================================
+
+# Zadanie 2.
+# Dana jest lista liczb całkowitych.
+# Znajdź najdłuższy spójny podciąg o największej sumie w tej liście.
+#
+# Na przykład dla listy [2, -4, 6, 8, -10, 100, -6, 5],
+# najdłuższy spójny podciąg o największej sumie to [6, 8, -10, 100].
+
+print("\nZadanie 2")
+
+input_arr = [2, -4, 6, 8, -10, 100, -6, 5]
+
+def zad2(arr: list[int]) -> list[int]:
+    max_sum = sum = arr[0]
+    start = end = start_t = 0
+    for i in range(1, len(arr)):
+        if sum + arr[i] < arr[i]:
+            sum = arr[i]
+            start_t = i
+        else:
+            sum += arr[i]
+        if sum > max_sum or (sum == max_sum and (i - start_t) > (end - start)):
+            max_sum = sum
+            start = start_t
+            end = i
+    return arr[start:end+1]
+
+print(zad2(input_arr))
+
+#===========================================================================
+
+# Zadanie 3.
+# Napisz program wypisujący wszystkie możliwe niemalejące ciągi składników
+# naturalnych (składniki mogą występować wielokrotnie), które w sumie dają
+# zadaną liczbę n.
+
+print("\nZadanie 3")
+
+def zad3(n: int) -> list[int]:
+    arr = []
+
+    def backtrack(curr: list, rest: int, start: int) -> None:
+        if rest == 0:
+            arr.append(curr[:])
+            return
+        for i in range(start, rest + 1):
+            curr.append(i)
+            backtrack(curr, rest - i, i)
+            curr.pop()
+
+    backtrack([], n, 1)
+    return arr
+
+for i in zad3(5):
+    print(i)
+
+#===========================================================================
+
+# Zadanie 4.
+# Wygeneruj wszystkie permutacje n początkowych liter alfabetu
+# i wypisz w porządku leksykograficznym.
+
+print("\nZadanie 4")
+
+def zad4(n: int) -> list[str]:
+    letters = [chr(97 + i) for i in range(n)]
+    final = []
+
+    def permutate(arr):
+        i = len(arr) - 2
+        while i >= 0 and arr[i] >= arr[i + 1]:
+            i -= 1
+        if i == -1:
+            return False
+
+        j = len(arr) - 1
+        while arr[j] <= arr[i]:
+            j -= 1
+        arr[i], arr[j] = arr[j], arr[i]
+
+        arr[i + 1:] = reversed(arr[i + 1:])
+        return True
+
+    final.append("".join(letters))
+    while permutate(letters):
+        final.append("".join(letters))
+    return final
+
+
+for i in zad4(3):
+    print(i)
+
+#===========================================================================
---
+++ test_main.py
@@ -0,0 +1,44 @@
+from main import *
+
+
+def test_zad1():
+    data = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+    result = zad1(data)
+    expected = [10, 22, 33, 50, 60, 80]
+    assert result == expected
+
+
+def test_zad2():
+    data = [2, -4, 6, 8, -10, 100, -6, 5]
+    result = zad2(data)
+    expected = [6, 8, -10, 100]
+    assert result == expected
+
+
+def test_zad3():
+    data = 5
+    result = zad3(data)
+    expected = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 2],
+        [1, 1, 3],
+        [1, 2, 2],
+        [1, 4],
+        [2, 3],
+        [5]
+    ]
+    assert result == expected
+
+
+def test_zad4():
+    data = 3
+    result = zad4(data)
+    expected = [
+        "abc",
+        "acb",
+        "bac",
+        "bca",
+        "cab",
+        "cba",
+    ]
+    assert result == expected
